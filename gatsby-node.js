598df13e5e4700751cf4db000c00db255f805473
Benedicte Raae <raae@raae.codes>
2022-01-11 11:15:19 +0100

    feat: ssg pages without data

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,37 +1,7 @@
-const axios = require("axios");
-
-const YOUTUBE_IDS = ["UGq8cnNTbwI", "fzlIzQbMtwM", "UsSJ_QNp6uo"];
-
-const fetchEmbed = async (id, logger) => {
-  try {
-    const {data} = await axios.get("https://www.youtube.com/oembed", {
-      params: {url: `https://youtu.be/${id}`, maxwidth: 800},
-    });
-    logger.info(`Fetched embed for ${id}: ${data.title}`);
-    return data;
-  } catch (error) {
-    logger.warn(`Error fetching embed for ${id}: ${error.message}`);
-  }
-};
-
-exports.sourceNodes = async ({
-  actions: {createNode},
-  createContentDigest,
-  reporter,
-}) => {
-  // Sourcing harcoded urls,
-  // this would typically come from a more dynamic source
-  for (const id of YOUTUBE_IDS) {
-    const embedData = await fetchEmbed(id, reporter);
-    if (!embedData) return;
-
-    createNode({
-      id: id,
-      ...embedData,
-      internal: {
-        type: "youTubeEmbed",
-        contentDigest: createContentDigest(id),
-      },
-    });
-  }
+exports.createPages = async function ({actions}) {
+  actions.createPage({
+    path: "yt-id",
+    component: require.resolve(`./src/templates/youtube-template.js`),
+    context: {id: "yt-id"},
+  });
 };
---
+++ src/pages/{YouTube.id}.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const YouTubeFileSystemApiPage = () => {
+  return (
+    <main>
+      <header>
+        <h1>TITLE OF VIDEO</h1>
+      </header>
+      <section>Embed HTML</section>
+    </main>
+  );
+};
+
+export default YouTubeFileSystemApiPage;
